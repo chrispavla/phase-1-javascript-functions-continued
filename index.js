4294405361b8463a016133401c6b86f474e39232
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-06-07 08:52:02 -0400

    commit tree 4b510377b9aff91e006777ab0fe67c5873025c85

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(tlag = "*") {
+    
+    return function(blag = "special") {
+        return `You are ${tlag}${blag}${tlag}!`
+    }
+}
+
+saturdayFun();
+mondayWork();
+const encouragingPromptFunction = wrapAdjective("!!!")
